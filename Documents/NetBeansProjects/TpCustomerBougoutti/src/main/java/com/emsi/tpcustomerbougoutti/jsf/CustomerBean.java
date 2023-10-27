f93def2b68171dfc790df1f04e668795f72ed75f
Bougoutti <salmaboug5@gmail.com>
2023-10-27 18:12:51 +0100

    Etat initial

---
+++ Documents/NetBeansProjects/TpCustomerBougoutti/src/main/java/com/emsi/tpcustomerbougoutti/jsf/CustomerBean.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.emsi.tpcustomerbougoutti.jsf;
+
+import com.emsi.tpcustomerbougoutti.entity.Customer;
+import com.emsi.tpcustomerbougoutti.service.CustomerManager;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author M2B PRO
+ */
+@Named(value = "customerBean")  
+@ViewScoped  
+public class CustomerBean implements Serializable {  
+  private List<Customer> customerList;  
+
+  @Inject
+  private CustomerManager customerManager;  
+        
+  public CustomerBean() {  }  
+        
+  /** 
+   * Retourne la liste des clients pour affichage dans une DataTable.
+   */  
+  public List<Customer> getCustomers() {
+    if (customerList == null) {
+      customerList = customerManager.getAllCustomers();
+    }
+    return customerList;
+  }  
+}
